7a238087cf78b465b899f9ea294d302d2a4477c5
akashMehta-STL <akash.mehta@solutelabs.com>
2019-01-31 21:29:39 +0530

    Revert "setup dependencies"
    This reverts commit 86b8195

--- app/src/main/java/aakash/com/voiceandmapassignment/common/endpoints/AppEndPoints.kt
+++
@@ -1,7 +0,0 @@
-package aakash.com.voiceandmapassignment.common.endpoints
-
-/**
- * Created by stllpt031 on 31/1/19.
- */
-interface AppEndPoints {
-}
--- app/src/main/java/aakash/com/voiceandmapassignment/mapView/MapsPresenter.kt
+++
@@ -1,5 +0,0 @@
-package aakash.com.voiceandmapassignment.mapView
-
-/**
- * Created by stllpt031 on 31/1/19.
- */
